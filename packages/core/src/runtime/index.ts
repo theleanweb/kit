5496d05f5f03a13ebaa3ee52b9b15aab65d96f12
Joshua Amaju <princejosh0007@gmail.com>
2023-09-06 23:09:24 +0100

    chore: export hono packages

--- packages/core/src/runtime/index.ts
+++ packages/core/src/runtime/index.ts
@@ -1,2 +1,7 @@
+export * from "hono";
+export * from "hono/cors";
+export * from "hono/cache";
+export * from "hono/cookie";
+export * from "hono/compress";
 export { render } from "./render.js";
 export { Hono as Router } from "hono/quick";
