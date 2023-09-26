f4558d9fa86a787a8a3e447c9e2106eb69aabe3f
Joshua Amaju <princejosh0007@gmail.com>
2023-09-26 20:53:21 +0100

    chore: update example

--- playground/basic/src/entry.ts
+++ playground/basic/src/entry.ts
@@ -1,25 +1,25 @@
 import { cookie } from "@hattip/cookie";
 import { session, SimpleCookieStore } from "@hattip/session";
 
-import { createRouter, render } from "leanweb-kit/runtime";
+import { Router, render } from "leanweb-kit/runtime";
 
-const app = createRouter();
+const app = new Router();
 
-app.use(cookie());
+// app.use(cookie());
 
-app.use(
-  session({
-    // Session store
-    store: new SimpleCookieStore(),
-    // Default session data when a new session is created.
-    // It can be a function.
-    // It is shallow cloned, if you need a deep clone, use a function.
-    defaultSessionData: { user: null },
-  })
-);
+// app.use(
+//   session({
+//     // Session store
+//     store: new SimpleCookieStore(),
+//     // Default session data when a new session is created.
+//     // It can be a function.
+//     // It is shallow cloned, if you need a deep clone, use a function.
+//     defaultSessionData: { user: null },
+//   })
+// );
 
 app.get("/", (ctx) => {
-  ctx.session.data = { user: "123" };
+  // ctx.session.data = { user: "123" };
   return new Response('Go to <a href="/about">About</a>', {
     headers: { "Content-Type": "text/html" },
   });
@@ -28,7 +28,7 @@ app.get("/", (ctx) => {
 app.get("/main", async () => render("main", {}));
 
 app.get("/home", async (ctx) => {
-  console.log(ctx.session.data);
+  // console.log(ctx.session.data);
   return render("home/home.html", {});
 });
 
