8fd3347f313ccb4c11d37c1c522b53d5c72f2575
Joshua Amaju <princejosh0007@gmail.com>
2023-09-26 20:52:53 +0100

    chore: export core runtime

--- packages/core/src/runtime/index.ts
+++ packages/core/src/runtime/index.ts
@@ -1,2 +1,17 @@
 export { render, renderToString } from "./internal/render.js";
-export * from "@hattip/router";
+export {
+  Hono as Router,
+  Context,
+  Env,
+  ErrorHandler,
+  Handler,
+  ContextVariableMap,
+  Input,
+  MiddlewareHandler,
+  Next,
+  NotFoundHandler,
+} from "hono";
+
+export * from "hono/cors";
+export * from "hono/cookie";
+export * from "hono/logger";
