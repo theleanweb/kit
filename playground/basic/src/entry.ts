c4b55ff62404b458871f408caa0318763f2119db
Joshua Amaju <princejosh0007@gmail.com>
2023-10-15 12:55:04 +0100

    chore

--- playground/basic/src/entry.ts
+++ playground/basic/src/entry.ts
@@ -1,39 +1,24 @@
-import { getCookie, setCookie } from "hono/cookie";
-import { render, Router, createSessionFactory } from "leanweb-kit/runtime";
-
-const { getSession, commitSession, destroySession } = createSessionFactory({
-  name: "user",
-  // domain: "remix.run",
-  // Expires can also be set (although maxAge overrides it when used in combination).
-  // Note that this method is NOT recommended as `new Date` creates only one date on each server deployment, not a dynamic date in the future!
-  //
-  // expires: new Date(Date.now() + 60_000),
-  httpOnly: true,
-  maxAge: 60,
-  path: "/",
-  sameSite: "Strict",
-  secure: true,
-});
+import { deleteCookie, getCookie, setCookie } from "hono/cookie";
+import { Router, view } from "leanweb-kit/runtime";
 
 const app = new Router();
 
-app.get("/", async (ctx) => {
-  await commitSession(ctx, { user: "123" });
-  setCookie(ctx, "age", "10");
+app.get("/", (ctx) => {
+  setCookie(ctx, "age", "20");
   return ctx.html('Go to <a href="/about">About</a>');
 });
 
-app.get("/main", async () => render("main", {}));
+app.get("/main", (ctx) => view(ctx, "main", {}));
 
-app.get("/home", async (ctx) => {
-  console.log(await getSession(ctx), getCookie(ctx, "age"));
-  return render("home/home.html", {});
+app.get("/home", (ctx) => {
+  console.log(getCookie(ctx, "age"));
+  return view(ctx, "home2/home.html", {});
 });
 
-app.get("/about", async (ctx) => render("about", {}));
+app.get("/about", (ctx) => view(ctx, "about", {}));
 
-app.get("/destroy", async (ctx) => {
-  destroySession(ctx);
+app.get("/destroy", (ctx) => {
+  deleteCookie(ctx, "age");
   return ctx.redirect("/");
 });
 
