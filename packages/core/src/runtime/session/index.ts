38275d211f4c6227c701659d37382c160e59099e
Joshua Amaju <princejosh0007@gmail.com>
2023-10-15 12:53:32 +0100

    chore: remove custom session implementation

--- packages/core/src/runtime/session/index.ts
+++
@@ -1,9 +0,0 @@
-import { createCookieFactory } from "./cookies.js";
-import { createSessionStorageFactory } from "./sessions.js";
-
-export const createCookie = createCookieFactory({ sign, unsign });
-
-export const createCookieSessionStorage =
-  createCookieSessionStorageFactory(createCookie);
-
-export const createSessionStorage = createSessionStorageFactory(createCookie);
