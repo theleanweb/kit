e7a6646591c2db03d4e95ec8c99bd00886c5eac1
Joshua Amaju <princejosh0007@gmail.com>
2023-11-05 13:14:35 +0100

    chore: make hono a peer dependency

--- packages/core/src/runtime/index.ts
+++ packages/core/src/runtime/index.ts
@@ -1,18 +1,1 @@
 export { view, render, viewToString } from "./internal/render.js";
-
-export {
-  Hono as Router,
-  Context,
-  Env,
-  ErrorHandler,
-  Handler,
-  ContextVariableMap,
-  Input,
-  MiddlewareHandler,
-  Next,
-  NotFoundHandler,
-} from "hono";
-
-export * from "hono/cors";
-export * from "hono/cookie";
-export * from "hono/logger";
